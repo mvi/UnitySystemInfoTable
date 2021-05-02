a6d918232162812aa34d8b0eea579aac6579f663
Barnaby MacBook <barnabysmith@gmail.com>
2021-05-02 13:15:07 +0100

    * Update the layout so that certain values are colour coded and also more important columns are earlier

--- createHtml.js
+++ createHtml.js
@@ -8,7 +8,7 @@ if (!fs.existsSync(dir)) {
 }
 
 // Track all the unique keys across different JSON files as new Unity versions add new entries to SystemInfo
-const keys = new Set();
+const uniqueKeys = new Set();
 
 // Read all the SystemInfo json files
 const systemInfos = [];
@@ -19,31 +19,62 @@ fs.readdirSync('input').forEach(file => {
     systemInfos.push(systemInfo);
 
     // Track the relevant unique keys
-    Object.keys(systemInfo).forEach(item => keys.add(item))
+    Object.keys(systemInfo).forEach(item => uniqueKeys.add(item))
 });
+
+let keys = Array.from(uniqueKeys);
+
+keys.splice(0, 0, keys.splice(keys.indexOf("unityPlatform"), 1)[0]);
+keys.splice(1, 0, keys.splice(keys.indexOf("deviceModel"), 1)[0]);
 
 // Convert the system infos into an HTML Table
 let tableHtml = "";
 
-tableHtml += "\t\t\t<tr>\n"
+tableHtml += "\t\t<tr>\n"
 
 for (const key of keys) {
-    tableHtml += "\t\t\t\t<th>" + key + "</th>\n";
+    tableHtml += "\t\t\t<th>" + key + "</th>\n";
 }
 
-tableHtml += "\t\t\t</tr>\n"
+tableHtml += "\t\t</tr>\n"
 
 for (const systemInfo of systemInfos) {
 
-    tableHtml += "\t\t\t<tr>\n"
+    tableHtml += "\t\t<tr>\n"
 
     for (const key of keys) {
-        tableHtml += "\t\t\t\t<td>" + systemInfo[key] + "</td>\n";
+        let displayValue = "";
+        let cssClass = null;
+
+        if (systemInfo[key] == null) {
+            displayValue = "no data in Unity version";
+            cssClass = "missing";
+        }
+        else {
+            if (systemInfo[key] === true) {
+                cssClass = "positive";
+            }
+            else if (systemInfo[key] === false) {
+                cssClass = "negative";
+            }
+            else if (systemInfo[key] == "REDACTED") {
+                cssClass = "redacted";
+            }
+
+            displayValue = systemInfo[key];
+        }
+
+        if (cssClass != null) {
+            tableHtml += `\t\t\t<td class='${cssClass}'>${displayValue}</td>\n`;
+        }
+        else {
+            tableHtml += `\t\t\t<td>${displayValue}</td>\n`;
+        }
     }
-    tableHtml += "\t\t\t</tr>\n"
+    tableHtml += "\t\t</tr>\n"
 }
 
 // Replace the templated section of the html file with our table html
 let html = fs.readFileSync('indexTemplate.html');
-html = html.toString().replace("\t\t\t#TABLE_REPLACE#\n", tableHtml);
+html = html.toString().replace("\t\t#TABLE_REPLACE#\n", tableHtml);
 fs.writeFileSync(dir + '/index.html', html);
